4222e968d252dc5b39bc6a170c8c6ceaf8667cbc
wll8 <xwdaysme@gmail.com>
2023-03-23 12:11:47 +0800

    feat: Avoid port hogging

--- server/index.js
+++ server/index.js
@@ -1,48 +1,58 @@
+#!/usr/bin/env node
+
 const path = require(`path`)
 const fs = require(`fs`)
 const express = require(`express`)
 const os = require(`os`)
 const util = require(`./util.js`)
 const normalize = require(`normalize-path`)
+const opener = require(`opener`)
 
-const server = express()
-server.use(express.json()) // for parsing application/json
-server.get(`/api/fileData`, (req, res) => {
-  let { historyPath, toDir } = req.body
-  // @see https://stackoverflow.com/a/72610691
-  const homeDir = os.userInfo().homedir
-  const pathList = [
-    historyPath,
-    `${homeDir}/AppData/Roaming/Code/User/History/`,
-    `${homeDir}/AppData/Roaming/Code - Insiders/User/History/`,
-    `${homeDir}/AppData/Roaming/VSCodium/User/History`,
-    `${homeDir}/.config/VSCodium/User/History/`,
-  ]
-  historyPath = (() => {
-    return pathList.find((path) => path && fs.existsSync(path))
-  })()
-  toDir = toDir || normalize(`${process.cwd()}/re-store/`)
-  if (historyPath) {
-    const fileList = util.scan({
-      historyPath: normalize(historyPath),
-      toDir,
-    })
-    res.json({
-      historyPath: normalize(historyPath),
-      toDir,
-      list: fileList,
-    })
-  } else {
-    res.json({ msg: `Wrong historyPath`, data: pathList })
-  }
-})
-server.post(`/api/getTime`, (req, res) => {
-  const { cache } = req.body
-  // todo 应处理编码格式
-  res.json({ text: require(`fs`).readFileSync(cache, `utf8`) })
-})
-server.use(express.static(path.join(__dirname, `../dist`)))
+void (async () => {
+  const { portNumbers, default: getPort } = await import(`get-port`)
+  const port = await getPort({ port: portNumbers(3000, 3100) })
+  const server = express()
+  server.use(express.json()) // for parsing application/json
+  server.get(`/api/fileData`, (req, res) => {
+    let { historyPath, toDir } = req.body
+    // @see https://stackoverflow.com/a/72610691
+    const homeDir = os.userInfo().homedir
+    const pathList = [
+      historyPath,
+      `${homeDir}/AppData/Roaming/Code/User/History/`,
+      `${homeDir}/AppData/Roaming/Code - Insiders/User/History/`,
+      `${homeDir}/AppData/Roaming/VSCodium/User/History`,
+      `${homeDir}/.config/VSCodium/User/History/`,
+    ]
+    historyPath = (() => {
+      return pathList.find((path) => path && fs.existsSync(path))
+    })()
+    toDir = toDir || normalize(`${process.cwd()}/re-store/`)
+    if (historyPath) {
+      const fileList = util.scan({
+        historyPath: normalize(historyPath),
+        toDir,
+      })
+      res.json({
+        historyPath: normalize(historyPath),
+        toDir,
+        list: fileList,
+      })
+    } else {
+      res.json({ msg: `Wrong historyPath`, data: pathList })
+    }
+  })
+  server.post(`/api/getTime`, (req, res) => {
+    const { cache } = req.body
+    // todo 应处理编码格式
+    res.json({ text: require(`fs`).readFileSync(cache, `utf8`) })
+  })
+  server.use(express.static(path.join(__dirname, `../dist`)))
 
-// 监听端口
-const port = `8081`
-server.listen(port, `0.0.0.0`, () => console.log(`service started: ${port}`))
+  // 监听端口
+  server.listen(port, `0.0.0.0`, () => {
+    const link = `http://127.0.0.1:${port}`
+    console.log(link)
+    opener(link)
+  })
+})()
